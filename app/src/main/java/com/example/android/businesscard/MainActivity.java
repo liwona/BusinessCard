14e38b97d177979a5ad3440a94d72597bdd248ac
liwona <liwona@users.noreply.github.com>
2018-02-16 21:57:36 +0100

    mainactivity_activitymain
    adding intents for phone call and email

--- app/src/main/java/com/example/android/businesscard/MainActivity.java
+++ app/src/main/java/com/example/android/businesscard/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.businesscard;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void phoneCall(View view) {
+        String phoneNumber = getString(R.string.phone_number);
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void sendEmail (View view) {
+        String emailAddress = getString(R.string.email_address);
+        String emailSubject = getString(R.string.email_subject);
+        String emailMessage = getString(R.string.email_message);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:" + emailAddress));
+        intent.putExtra(Intent.EXTRA_SUBJECT, emailSubject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 }
